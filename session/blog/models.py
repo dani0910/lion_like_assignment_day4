41f9e33319070c9b534c464cb75a000ce1ba07c4
dani0910 <dlekdms0910@naver.com>
2024-03-28 00:32:58 +0900

    day4

---
+++ session/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 20)
+    content = models.TextField()
+    writer = models.CharField(max_length = 20)
+    createdAt = models.DateTimeField(auto_now_add = True)
+    email = models.EmailField(max_length = 25, default = "")
+
+    def __str__(self):
+        return self.title
+# Create your models here.
